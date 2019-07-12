f35d88c41417abd7761f5213e3499256a4f4d0ad
sguthals <sarah@guthals.com>
2019-07-11 18:23:25 -0700

    File uploads

---
+++ 2019/july/11/ingenious_turing_wluff1.ino
@@ -0,0 +1,44 @@
+void setup()
+{
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(A0, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A0, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A3, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A3, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(A1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
