9e519aab5d992bfa47097d630995eecfbbc617ab
Sarah Guthals <sarah@guthals.com>
2019-07-12 19:59:55 -0700

    Update ingenious_turing_wluff1.ino

--- 2019/july/11/ingenious_turing_wluff1.ino
+++ 2019/july/11/ingenious_turing_wluff1.ino
@@ -2,16 +2,19 @@ void setup()
 {
   pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
+  pinMode(1, OUTPUT);
   pinMode(A2, OUTPUT);
+  pinMode(2, OUTPUT);
   pinMode(A3, OUTPUT);
+  pinMode(3, OUTPUT);
   pinMode(A4, OUTPUT);
 }
 
 void loop()
 {
-  jumpRope();
+  jumpRope()
 }
-
+  
 void jumpRope()
 {
   digitalWrite(A0, HIGH);
@@ -19,16 +22,22 @@ void jumpRope()
   digitalWrite(A0, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A1, HIGH);
+  digitalWrite(1, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A1, LOW);
+  digitalWrite(1, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A2, HIGH);
+  digitalWrite(2, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A2, LOW);
+  digitalWrite(2, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A3, HIGH);
+  digitalWrite(3, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A3, LOW);
+  digitalWrite(3, LOW);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A4, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
@@ -45,5 +54,5 @@ void jumpRope()
   digitalWrite(A1, HIGH);
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(A1, LOW);
-  delay(1000); // Wait for 1000 millisecond(s) 
+  delay(1000); // Wait for 1000 millisecond(s)
 }
